4e4226f5fc497f0676ad94a03e814f83cc63fe73
Slava Pravda <pravdaslava7@gmail.com>
2022-08-29 21:28:10 +0300

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,63 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 
-export class App extends Component {
-  state = {
-    searchName: '',
-    page: 1,
+export const App = () => {
+  const [searchName, setName] = useState('');
+  const [page, setPage] = useState(1);
+
+  const formSearchHandler = ({ searchName }) => {
+    console.log(searchName);
+    setName(searchName);
+    setPage(1);
   };
 
-  formSearchHandler = searchName => {
-    this.setState({
-      searchName,
-      page: 1,
-    });
-  };
+  const onLoadMoreBtnClick = () => setPage(prevPage => prevPage + 1);
 
-  onLoadMoreBtnClick = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
+  return (
+    <div>
+      <Searchbar onSubmit={formSearchHandler} searchName={searchName} />
+      <ImageGallery
+        searchName={searchName}
+        page={page}
+        onLoadMoreBtnClick={onLoadMoreBtnClick}
+      />
+    </div>
+  );
+};
 
-  render() {
-    const { formSearchHandler, onLoadMoreBtnClick } = this;
-    const { searchName, page } = this.state;
+// export class App extends Component {
+//   state = {
+//     searchName: '',
+//     page: 1,
+//   };
 
-    return (
-      <div>
-        <Searchbar onSubmit={formSearchHandler} searchName={searchName} />
-        <ImageGallery
-          searchName={searchName}
-          page={page}
-          onLoadMoreBtnClick={onLoadMoreBtnClick}
-        />
-      </div>
-    );
-  }
-}
+//   formSearchHandler = searchName => {
+//     this.setState({
+//       searchName,
+//       page: 1,
+//     });
+//   };
+
+//   onLoadMoreBtnClick = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   render() {
+//     const { formSearchHandler, onLoadMoreBtnClick } = this;
+//     const { searchName, page } = this.state;
+
+//     return (
+//       <div>
+//         <Searchbar onSubmit={formSearchHandler} searchName={searchName} />
+//         <ImageGallery
+//           searchName={searchName}
+//           page={page}
+//           onLoadMoreBtnClick={onLoadMoreBtnClick}
+//         />
+//       </div>
+//     );
+//   }
+// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,73 +1,126 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import s from './Searchbar.module.css';
 
 import Notiflix from 'notiflix';
 
-class Searchbar extends Component {
-  state = {
-    searchName: '',
+const Searchbar = ({ onSubmit }) => {
+  const [searchName, setName] = useState('');
+
+  const handeChange = event => {
+    setName(event.target.value);
   };
 
-  handeChange = e => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const { searchName } = this.state;
 
-    if (searchName.trim() === '') {
+    if (searchName === '') {
       return Notiflix.Notify.info('Введіть коректну назву');
     }
 
-    if (searchName.trim() === this.props.searchName) {
-      return Notiflix.Notify.failure('Введіть іншу назву пошуку');
-    }
-
-    this.props.onSubmit(searchName);
-
-    this.reset();
+    onSubmit({ searchName });
+    // reset();
+    setName('');
   };
 
-  reset = () => {
-    this.setState({
-      searchName: '',
-    });
-  };
+  // const reset = () => {
+  //   setName({
+  //     searchName: '',
+  //   });
+  // };
 
-  render() {
-    const { handeChange, handleSubmit } = this;
-    const { searchName } = this.state;
-    return (
-      <header className={s.Searchbar}>
-        <form className={s.SearchForm} onSubmit={handleSubmit}>
-          <button type="submit" className={s.SearchFormButton}>
-            <span className={s.SearchFormButtonLabel}>Search</span>
-          </button>
+  return (
+    <header className={s.Searchbar}>
+      <form className={s.SearchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={s.SearchFormButton}>
+          <span className={s.SearchFormButtonLabel}>Search</span>
+        </button>
 
-          <input
-            value={searchName}
-            onChange={handeChange}
-            className={s.SearchFormInput}
-            name="searchName"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          onChange={handeChange}
+          className={s.SearchFormInput}
+          name="searchName"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
 
 export default Searchbar;
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
+// class Searchbar extends Component {
+
+//   state = {
+//     searchName: '',
+//   };
+
+//   handeChange = e => {
+//     const { name, value } = e.target;
+//     this.setState({
+//       [name]: value,
+//     });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const { searchName } = this.state;
+
+//     if (searchName.trim() === '') {
+//       return Notiflix.Notify.info('Введіть коректну назву');
+//     }
+
+//     if (searchName.trim() === this.props.searchName) {
+//       return Notiflix.Notify.failure('Введіть іншу назву пошуку');
+//     }
+
+//     this.props.onSubmit(searchName);
+
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({
+//       searchName: '',
+//     });
+//   };
+
+//   render() {
+//     const { handeChange, handleSubmit } = this;
+//     const { searchName } = this.state;
+//     return (
+//       <header className={s.Searchbar}>
+//         <form className={s.SearchForm} onSubmit={handleSubmit}>
+//           <button type="submit" className={s.SearchFormButton}>
+//             <span className={s.SearchFormButtonLabel}>Search</span>
+//           </button>
+
+//           <input
+//             value={searchName}
+//             onChange={handeChange}
+//             className={s.SearchFormInput}
+//             name="searchName"
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Searchbar;
+
+// Searchbar.propTypes = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
